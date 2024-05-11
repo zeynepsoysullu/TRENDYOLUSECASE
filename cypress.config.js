53d72c2150f9d5ed9c76a3aa0ff9c0229e1b1369
zeynepsoysullu <zeynepsoysullu@gmail.com>
2024-05-11 19:22:57 +0300

    -

--- cypress.config.js
+++ cypress.config.js
@@ -13,19 +13,10 @@
 // });
 
 
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-    "spec": {
-        "e2e": {
-            "setupNodeEvents": (on, config) => {
-                return require('./cypress/plugins')(on, config)
-            },
-            "specPattern": "cypress/e2e/BDD-Features",
-            "excludeSpecPattern": ["*.js", "*.md"]
-        }
+module.exports = {
+    e2e: {
+        setupNodeEvents: true,
+        specPattern: 'cypress/e2e/BDD-Features',
+        excludeSpecPattern: ['*.js', '*.md']
     }
-
-
-
-});
+};
