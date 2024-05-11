799d6f508844b70a40d4d6d28ccbcab30e0f613b
zeynepsoysullu <zeynepsoysullu@gmail.com>
2024-05-11 19:28:40 +0300

    -

--- cypress/plugin/index.js
+++ cypress/plugin/index.js
@@ -8,14 +8,11 @@
 // }
 
 
+
+
+const cucumber = require('cypress-cucumber-preprocessor').default;
+const cypress = require("cypress");
+
 module.exports = (on, config) => {
-    const cucumber = require('cypress-cucumber-preprocessor').default;
     on('file:preprocessor', cucumber());
 };
-
-// const cucumber = require('cypress-cucumber-preprocessor').default;
-// const cypress = require("cypress");
-
-// module.exports = (on, config) => {
-//     on('file:preprocessor', cucumber());
-// };
